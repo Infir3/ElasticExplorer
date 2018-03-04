487fa3884c23f1470179e38e11f26adb6a2ce91e
Stefan <stefanbecking@gmail.com>
2018-03-04 14:04:46 +0100

    randomize reporting data, try out graph java api

--- src/main/java/com/sb/elastic/GraphController.java
+++ src/main/java/com/sb/elastic/GraphController.java
@@ -1,11 +1,16 @@
 package com.sb.elastic;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sb.elastic.model.Measure;
 import com.sb.elastic.model.Report;
 import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.Random;
 import org.apache.http.HttpHost;
+import org.apache.http.util.EntityUtils;
 import org.elasticsearch.ElasticsearchStatusException;
 import org.elasticsearch.action.admin.indices.create.CreateIndexRequest;
 import org.elasticsearch.action.admin.indices.create.CreateIndexResponse;
@@ -15,9 +20,20 @@ import org.elasticsearch.action.bulk.BulkItemResponse;
 import org.elasticsearch.action.bulk.BulkRequest;
 import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.client.Response;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.TransportAddress;
 import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.xpack.client.PreBuiltXPackTransportClient;
+import org.elasticsearch.xpack.core.graph.action.GraphExploreAction;
+import org.elasticsearch.xpack.core.graph.action.GraphExploreRequestBuilder;
+import org.elasticsearch.xpack.core.graph.action.GraphExploreResponse;
+import org.elasticsearch.xpack.core.graph.action.Hop;
+import org.elasticsearch.xpack.core.graph.action.Vertex;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -31,9 +47,9 @@ public class GraphController {
 
     private static final String INDEX_REPORTING_DATA = "reporting_data";
 
-    @RequestMapping("/createTemplateIndex")
+    @RequestMapping("/createReportingData")
     @ResponseBody
-    public void createTemplateIndex() throws IOException {
+    public void createReportingData() throws IOException {
         RestHighLevelClient client = this.getRemoteClient();
 
         try {
@@ -46,6 +62,39 @@ public class GraphController {
         CreateIndexRequest createIndexRequest = new CreateIndexRequest(INDEX_REPORTING_DATA);
         CreateIndexResponse createIndexResponse = client.indices().create(createIndexRequest);
 
+//        List<Report> reportList = this.createReportList();
+        List<Report> reportList = this.createReportListRandomized();
+
+        // instance a json mapper
+        ObjectMapper mapper = new ObjectMapper(); // create once, reuse
+
+        BulkRequest request = new BulkRequest();
+        for (Report report : reportList) {
+            String stringifiedJson = mapper.writeValueAsString(report);
+            request.add(new IndexRequest(INDEX_REPORTING_DATA, "report")
+                .source(stringifiedJson, XContentType.JSON));
+        }
+
+        // this provokes an ElasticsearchException because more than 1 type per
+        // mapping is not allowed
+//        for (Measure measure : this.createMeasureList()) {
+//            String stringifiedJson = mapper.writeValueAsString(measure);
+//            request.add(new IndexRequest(INDEX_REPORTING_DATA, "measure")
+//                .source(stringifiedJson, XContentType.JSON));
+//        }
+
+        BulkResponse bulkResponse = client.bulk(request);
+        for (BulkItemResponse bulkItemResponse : bulkResponse) {
+            if (bulkItemResponse.isFailed()) {
+                BulkItemResponse.Failure failure = bulkItemResponse.getFailure();
+                LOG.error(failure.getMessage());
+            }
+        }
+
+        client.close();
+    }
+
+    private List<Report> createReportList() {
         List<Report> reportList = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
             String title = "temp" + i;
@@ -68,30 +117,108 @@ public class GraphController {
         reportList.get(4).getMeasures().add(3);
 
         reportList.get(5).getMeasures().add(3);
+        return reportList;
+    }
 
-        // instance a json mapper
-        ObjectMapper mapper = new ObjectMapper(); // create once, reuse
+    private List<Report> createReportListRandomized() {
+        List<Report> reportList = new ArrayList<>();
 
-        BulkRequest request = new BulkRequest();
-        for (Report report : reportList) {
-            String stringifiedJson = mapper.writeValueAsString(report);
-            request.add(new IndexRequest(INDEX_REPORTING_DATA, "report")
-                .source(stringifiedJson, XContentType.JSON));
+        Random random = new Random();
+        for (int i = 0; i < 100; i++) {
+            String title = "temp" + i;
+            String description = "description" + i;
+            Report report = new Report(title, description);
+//            int numberOfMeasures = random.nextInt(10);
+            int numberOfMeasures = this.getBinomial(10, 0.5);
+            for (int j = 0; j < numberOfMeasures; j++) {
+                int measure = this.getBinomial(10, 0.5);
+//                int measure = random.nextInt(10) + 1;
+                report.getMeasures().add(measure);
+            }
+            reportList.add(report);
         }
 
-        BulkResponse bulkResponse = client.bulk(request);
-        for (BulkItemResponse bulkItemResponse : bulkResponse) {
-            if (bulkItemResponse.isFailed()) {
-                BulkItemResponse.Failure failure = bulkItemResponse.getFailure();
-                LOG.error(failure.getMessage());
+        return reportList;
+    }
+
+    @RequestMapping("/queryReportingData")
+    @ResponseBody
+    public void queryReportingData() {
+        TransportClient client = new PreBuiltXPackTransportClient(Settings.EMPTY)
+            .addTransportAddress(new TransportAddress(new InetSocketAddress("localhost", 9300)));
+
+        int requiredNumberOfSightings = 1;
+
+        GraphExploreRequestBuilder grb = new GraphExploreRequestBuilder(client,
+            GraphExploreAction.INSTANCE).setIndices(INDEX_REPORTING_DATA);
+
+        Hop hopNew = grb.createNextHop(QueryBuilders.termQuery("_all", "*"));
+        hopNew.addVertexRequest("title.keyword").size(10).minDocCount(requiredNumberOfSightings);
+        GraphExploreResponse response = grb.get();
+
+        Hop hop1 = grb.createNextHop(QueryBuilders.termQuery("_all", "elasticsearch"));
+        // elasticsearch meetup attendees
+        hop1.addVertexRequest("member_id").size(10).minDocCount(requiredNumberOfSightings);
+        // groups attended by elastic attendees
+        grb.createNextHop(null).addVertexRequest("group_id").size(10)
+            .minDocCount(requiredNumberOfSightings);
+
+        response = grb.get();
+        Collection<Vertex> vertices = response.getVertices();
+
+        System.out.println("==Members===");
+        for (Vertex vertex : vertices) {
+            if (vertex.getField().equals("member_id")) {
+                System.out.println(vertex.getTerm());
             }
         }
+
+        System.out.println("==Groups===");
+        for (Vertex vertex : vertices) {
+            if (vertex.getField().equals("group_id")) {
+                System.out.println(vertex.getTerm());
+            }
+        }
+
         client.close();
+    }
+
+    @RequestMapping("/queryReportingDataRest")
+    @ResponseBody
+    public void queryReportingDataRest() throws IOException {
+        RestClient restClient = RestClient.builder(
+            new HttpHost("localhost", 9200, "http"),
+            new HttpHost("localhost", 9201, "http")).build();
+        Response response = restClient.performRequest("GET", "/");
+        String responseBody = EntityUtils.toString(response.getEntity());
+
     }
 
     private RestHighLevelClient getRemoteClient() {
         return new RestHighLevelClient(
             RestClient.builder(
                 new HttpHost("localhost", 9200, "http")));
+    }
+
+    private List<Measure> createMeasureList() {
+        List<Measure> measureList = new ArrayList<>();
+        measureList.add(new Measure(1, "tolle Kennzahl", "Beschreibung der tollen Kennzahl"));
+        measureList
+            .add(new Measure(2, "langweilige Kennzahl", "Beschreibung der langweilige Kennzahl"));
+        measureList.add(new Measure(3, "dumme Kennzahl", "Beschreibung der dummen Kennzahl"));
+        return measureList;
+    }
+
+    private int getBinomial(int n, double p) {
+        double log_q = Math.log(1.0 - p);
+        int x = 0;
+        double sum = 0;
+        for(;;) {
+            sum += Math.log(Math.random()) / (n - x);
+            if(sum < log_q) {
+                return x;
+            }
+            x++;
+        }
     }
 }
